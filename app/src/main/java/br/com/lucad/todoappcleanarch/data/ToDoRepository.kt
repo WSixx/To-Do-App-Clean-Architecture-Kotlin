90fb6369b7fc0931b6190c6922697cff60578699
WSixx <lucas.2007s@gmail.com>
2021-06-06 08:51:55 -0300

    Repository

---
+++ app/src/main/java/br/com/lucad/todoappcleanarch/data/ToDoRepository.kt
@@ -0,0 +1,12 @@
+package br.com.lucad.todoappcleanarch.data
+
+import androidx.lifecycle.LiveData
+
+class ToDoRepository(private val toDoDao: ToDoDao) {
+
+    val getAllData: LiveData<List<ToDoData>> = toDoDao.getAllData()
+
+    suspend fun insertData(toDoData: ToDoData){
+        toDoDao.insertData(toDoData)
+    }
+}
